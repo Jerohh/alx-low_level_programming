88119a7d4a969a688ec89f0c732d47ba96f1c735
jerry <ifeanyichukwujerry2019@gmail.com>
2022-09-15 01:14:52 -0700

    4th Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-/**
- * main - Print in C
- * Description: Prints "Programming is like building a multilingual puzzle"
- * Return: Always 0 (C Successful)
- */
-int main (void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-	return (0);
-}
++ #include <stdio.h>
++ /**
++ * main - Print in C
++ * Description: Prints "Programming is like building a multilingual puzzle"
++ * Return: Always 0 (C Successful)
++ */
++ int main (void)
++ {
++	puts("\"Programming is like building a multilingual puzzle");
++	return (0);
++	return (0);
++ }
 
