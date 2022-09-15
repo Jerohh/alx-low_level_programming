8ba9c92a495cf24de619dcb6675203ca1f2340fc
jerry <ifeanyichukwujerry2019@gmail.com>
2022-09-15 02:32:04 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,13 @@
 
 
 /**
- *  main - Print in C
+ *  main - Entry point
+ *  Description: Prints "Programming is like building a multilingual puzzle"
  *  
  *  Return: Returns 0
  *
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
