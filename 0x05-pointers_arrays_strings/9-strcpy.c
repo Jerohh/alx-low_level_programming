a1315cf05d472ef716bad6fc681bfe8b93f9b532
jerry <ifeanyichukwujerry2019@gmail.com>
2022-09-19 14:52:16 -0700

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strcpy - Copy paste string
- * @dest: destination
- * @src: source
- *
- * Return: destination
+* _strcpy - Copy paste string
+* @dest: destination
+* @src: source
+*
+* Return: destination
 */
 
 char *_strcpy(char *dest, char *src)
