057ada9d2bac76ce9f249c5699bb9a852bd121d5
jerry <ifeanyichukwujerry2019@gmail.com>
2022-09-23 13:59:01 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,7 +2,7 @@
 
 /**
  * leet - encodes a string into 1337
- *  @s: string to encode
+ * @s: string to encode
  *
  * Return: address of s
  */
@@ -17,7 +17,7 @@ char *leet(char *s)
 		for (j = 0; j <= 9; j++)
 		{
 			if (a[j] == *(s + i))
-				*(s + i) = b[j];
+			      *(s + i) = b[j];
 		}
 	}
 	return (s);
