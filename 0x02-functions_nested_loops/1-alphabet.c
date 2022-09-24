0fe1055d0592aa112908e8ad6d9b01e31f2a6b13
jerry <ifeanyichukwujerry2019@gmail.com>
2022-09-24 15:11:16 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * return:0
+ *
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+}
