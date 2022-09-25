e07e16797f3582c96aefbf35406fc50f000a2163
jerry <ifeanyichukwujerry2019@gmail.com>
2022-09-24 17:07:00 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: the int to extract the last digit from
- * @n: the int to extract the last digit from
+ * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
